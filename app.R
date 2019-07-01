c4fea2862c38249c4030a39070375cff7cf16b80
TPeschel <TPeschel@IMISE.Uni-Leipzig.DE>
2019-07-01 16:30:21 +0200

    add some pkgs

--- app.R
+++ app.R
@@ -1,11 +1,12 @@
 rm( list = ls( ) )
 
-neccessary.pkgs <- c( "dk", "DT", "markdown", "plotrix", "plotly", "rhandsontable", "shiny", "shinycssloaders", "shinydashboard", "stringr" )
+neccessary.pkgs <- c( "dk", "DT", "markdown", "plotrix", "plotly", "rhandsontable", "shiny", "shinycssloaders", "shinydashboard", "stringr", "dplyr" )
 
 installed.pkgs <- rownames( installed.packages( ) )
 
 install.packages( setdiff( neccessary.pkgs, installed.pkgs ) )
 
+library( "dplyr" )
 library( "DT" )
 library( "markdown" )
 library( "plotly" )
